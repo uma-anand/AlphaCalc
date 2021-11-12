6e6e8a6d70a0474350c8449affee347f59a1789d
sreya-sriram <91830477+sreya-sriram@users.noreply.github.com>
2021-11-12 12:19:45 +0530

    Update home.py

--- home.py
+++ home.py
@@ -67,11 +67,23 @@ class Ui_LoginWindow(object):
         self.submit.setText(_translate("LoginWindow", "Submit"))
 
     def login(self):
+        from calculator import Ui_CalcWindow
         try:
-            ms.connect(host = "localhost", user = "root", passwd = self.pwd_input.text())
+            db=ms.connect(host = "localhost", user = "root", passwd = self.pwd_input.text(),database='alphacalc')
             self.window = QtWidgets.QMainWindow()
             self.ui = Ui_CalcWindow()
             self.ui.setupUi(self.window)
+            cur = db.cursor()
+            cur.execute('select * from memory')
+            data=cur.fetchall()
+            if any(data):
+                for x in data:
+                        row_count=self.ui.Mem_Display.rowCount()
+                        self.ui.Mem_Display.setRowCount(row_count+1)
+                        self.ui.Mem_Display.setItem(row_count,0,QtWidgets.QTableWidgetItem(str(x[0])))
+                        self.ui.Mem_Display.setItem(row_count,1,QtWidgets.QTableWidgetItem(str(x[1])))
+                        self.ui.Mem_Display.setItem(row_count,2,QtWidgets.QTableWidgetItem(x[2]))
+                        self.ui.Mem_Display.setItem(row_count,3,QtWidgets.QTableWidgetItem(str(x[3])))
             self.window.show()
             LoginWindow.hide()
         except Exception:
